fe3db443c4e5d2b8a16f4ca491cc4917e0a34b9b
maxudong <44560196+dongdong863@users.noreply.github.com>
2019-07-18 19:13:05 +0800

    20190718 readConfig

---
+++ testFile/readConfig.py
@@ -0,0 +1,27 @@
+import os
+import configparser #读取配置文件的包
+import getpathInfo#导入提示报错，可以尝试把getpathInfo.py文件放到lib目录下
+
+path = getpathInfo.get_path()
+config_path = os.path.join(path,'config.ini')
+config = configparser.ConfigParser()
+config.read(config_path,encoding='utf-8')
+
+class ReadConfig():
+    
+    def get_http(self,name):
+        value= config.get('HTTP',name)
+        return value
+    def get_email(self,name):
+        value = config.get('EMAIL',name)
+        return value
+    def get_db(self,name):
+        value = config.get('DATABASE',name)
+        return value
+
+if __name__ == '__main':
+if __name__ == '__main__':
+    print('HTTP中的baseurl值为：',ReadConfig().get_http('baseurl'))
+    
+    print('EMAIL中开关on_off值为：',ReadConfig().get_email('on_off'))
+
