7de9a372e172dd77d52f11f23dbdadeaf49d3492
maxudong <44560196+dongdong863@users.noreply.github.com>
2019-07-20 14:28:09 +0800

    add readExcel.py

--- getpathInfo.py
+++ getpathInfo.py
@@ -1,8 +1,9 @@
 import os
 #os.path.split() 返回文件的路径和文件名
 def get_path():
-    path= os.path.split(os.path.realpath(__file__))[0]
-    #path = os.path.split(__file__)[0]
+
+    #path = os.path.split(os.path.realpath(__file__))[0]
+    path = os.path.split(__file__)[0]
     return path
 
 if __name__=='__main__':
--- readExcel.py
+++ readExcel.py
@@ -5,9 +5,8 @@ from xlrd import open_workbook
 path = getpathInfo.get_path()
 
 class readExcel():
-    def get_xls(self,xls_name,sheet_name):
+    def get_xls(self, xls_name, sheet_name):  # xls_name填写用例的Excel名称 sheet_name该Excel的sheet名称
         cls = []
-        
         xlsPath = os.path.join(path,"testFile",'case',xls_name)
         file = open_workbook(xlsPath)
         sheet = file.sheet_by_name(sheet_name)
@@ -20,6 +19,6 @@ class readExcel():
                 return cls
 
 if __name__ == '__main__':
-    print(readExcel(),get_xls('userCase.xlsx','login'))
-    print(readExcel(),get_xls('userCase.xlsx', 'login')[0][1])
-    print(readExcel(),get_xls('userCase.xlsx', 'login')[1][2])
+    print(readExcel().get_xls('userCase.xlsx', 'login'))
+    print(readExcel().get_xls('userCase.xlsx', 'login')[0][1])
+    print(readExcel().get_xls('userCase.xlsx', 'login')[1][2])
