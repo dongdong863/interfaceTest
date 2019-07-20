dbc21dee43169d8b8dcd3104981d7601ef1b9ca7
maxudong <44560196+dongdong863@users.noreply.github.com>
2019-07-20 14:34:52 +0800

    add readExcel.py

--- getpathInfo.py
+++ getpathInfo.py
@@ -2,8 +2,8 @@ import os
 #os.path.split() 返回文件的路径和文件名
 def get_path():
 
-    #path = os.path.split(os.path.realpath(__file__))[0]
-    path = os.path.split(__file__)[0]
+    path = os.path.split(os.path.realpath(__file__))[0]
+    #path = os.path.split(__file__)[0]
     return path
 
 if __name__=='__main__':
