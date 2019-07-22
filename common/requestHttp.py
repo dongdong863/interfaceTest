27bd00c3ec2e1cc58b1aa91fe7f1212d931a9bfb
maxudong <44560196+dongdong863@users.noreply.github.com>
2019-07-22 13:33:36 +0800

    add requestHttp.py

--- common/requestHttp.py
+++ common/requestHttp.py
@@ -1,11 +1,28 @@
-import request
+import requests
 import json
 class RunMain():
     def send_post(self, url, data):
+        result = requests.post(url= url,data= data).json()
+        res = json.dumps(result,ensure_ascii=False,sort_keys=True, indent=2)
+        return res
+    
+    def send_get(self,url,data):
+        result = requests.get(url= url,data=data)
+        res = json.dumps(result,ensure_ascii=False,sort_keys=True, indent=2)
+        return res
+    
+    def run_main(self,method,url=None,data=None):
+        result = None
+        if method =='post':
+            result= self.send_post(url,data)
+        elif method =='get':
+            result= self.send_get(url,data)
+        else:
+            print("menthod值错误！！")
         
-        result = request.post(url= url,data= data).json()
-
+        return result
 
 if __name__ == '__main__':
-    print（）
+    result = RunMain().run_main('post','http://127.0.0.1:8888/login','name=xiaoming&pwd=')
+    print(result)
 
